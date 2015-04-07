438261940bde91ffa4937e9a13811315e73bf4b0
Vinicius Carvalho <viniciusccarvalho@gmail.com>
2015-04-06 22:45:53 -0400

    Changing to multiproject

---
+++ stream-aggregator/src/main/java/us/devguild/rxfire/Application.java
@@ -0,0 +1,34 @@
+package us.devguild.rxfire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.*;
+import org.springframework.data.gemfire.CacheFactoryBean;
+import org.springframework.data.gemfire.repository.config.EnableGemfireRepositories;
+
+/**
+ * Created by vcarvalho on 12/28/14.
+ */
+
+@SpringBootApplication
+@EnableGemfireRepositories
+@ImportResource("cache-context.xml")
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication application = new SpringApplication(Application.class);
+        application.run(args);
+    }
+
+
+    @Bean
+    CacheFactoryBean cacheFactoryBean() {
+        return new CacheFactoryBean();
+    }
+
+    
+    
+
+
+}
---
+++ stream-aggregator/src/main/java/us/devguild/rxfire/model/Stock.java
@@ -0,0 +1,12 @@
+package us.devguild.rxfire.model;
+
+import java.io.Serializable;
+
+import org.springframework.data.annotation.Id;
+
+public class Stock implements Serializable{
+
+	@Id
+	private String id;
+	
+}
